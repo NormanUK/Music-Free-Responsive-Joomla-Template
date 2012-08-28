f74ee111e5064e1bca17dbba40325fa1531072ef
normanuk <normanuk@live.co.uk>
2012-08-28 09:11:26 +0100

    Added Countdown feature while site is in offline mode.

--- gk_music_free/offline.php
+++ gk_music_free/offline.php
@@ -35,6 +35,10 @@ if(($logo_image == '') || ($templateParams->get('logo_type') == 'css')) {
 $logo_text = $templateParams->get('logo_text', '');
 $logo_slogan = $templateParams->get('logo_slogan', '');
 
+// get Countdown date configuration
+$onlinedate = $templateParams->get('countdown_date', '');
+$endDate = date("M j, Y H:i:s", strtotime($onlinedate));
+
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
@@ -42,12 +46,60 @@ $logo_slogan = $templateParams->get('logo_slogan', '');
 <jdoc:include type="head" />
 <link href="http://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel="stylesheet" type="text/css" />
 <link rel="stylesheet" href="<?php echo JURI::base(); ?>templates/<?php echo $this->template; ?>/css/system/offline.style<?php echo $template_style; ?>.css" type="text/css" />
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+<script>
+jQuery(document).ready(function(e) {
+
+	gkCountdown();
+
+});
+function gkCountdown() {
+	var enddate = new Date("<?php echo $endDate?>");
+	var now = new Date();
+	var diff = enddate.getTime() - now.getTime();
+	var d = Math.floor(diff / 1000);
+	var l = Math.floor(d / 60);
+	var b = Math.floor(l / 60);
+	var u = Math.floor(b / 24);
+	b %= 24; l %= 60; d %= 60;
+		
+	if(d < 0){ var d = 0}
+	if(l < 0){ var l = 0}
+	if(b < 0){ var b = 0}
+	if(u < 0){ var u = 0}
+		
+	document.getElementById("gkDays").innerHTML = u;
+	document.getElementById("gkHours").innerHTML = b;
+	document.getElementById("gkMinutes").innerHTML = l;
+	document.getElementById("gkSeconds").innerHTML = d;
+	
+	var timer = setTimeout('gkCountdown()',1000);
+}
+</script>
 </head>
 <body><jdoc:include type="message" />
 	<div id="gkPage">		
 		<div id="gkPageWrap">
 			<h2><?php echo $app->getCfg('offline_message'); ?></h2>
-			
+			<div id="gkCounterWrap">
+				<div class="countertext">
+					<div class="numbers">
+						<div class="datetext days"><?php echo JText::_('TPL_GK_LANG_COUNTDOWN_DAYS'); ?></div><div id="gkDays"></div>
+					</div>
+					<div class="numbers">
+						<div class="datetext hours"><?php echo JText::_('TPL_GK_LANG_COUNTDOWN_HOURS'); ?></div><div id="gkHours"></div>
+					</div>
+					<div class="numbers">
+						<div class="datetext minutes"><?php echo JText::_('TPL_GK_LANG_COUNTDOWN_MINUTES'); ?></div><div id="gkMinutes"></div>
+					</div>
+					<div class="numbers">
+						<div class="datetext seconds"><?php echo JText::_('TPL_GK_LANG_COUNTDOWN_SECONDS'); ?></div><div id="gkSeconds"></div>
+					</div>
+				</div>
+			</div>	
+			<div id="message">
+				<h2><?php echo JText::_('TPL_GK_LANG_COUNTDOWN_OFFLINE_MSG'); ?><?php echo $onlinedate; ?></h2>
+			</div>			
 			<div id="left">
 				<?php if ($logo_type !== 'none' && !$app->getCfg('offline_image')): ?>
 				<?php if($logo_type == 'css') : ?>
